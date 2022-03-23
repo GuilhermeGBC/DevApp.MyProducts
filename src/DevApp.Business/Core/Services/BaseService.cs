7145e83d6edd20bc133e98065876f6fafedc8fba
Guilherme Bastos <gbcguilherme@hotmail.com>
2022-03-23 18:38:21 -0300

    Camada de negócios "finalizada"

--- src/DevApp.Business/Core/Services/BaseService.cs
+++ src/DevApp.Business/Core/Services/BaseService.cs
@@ -1,23 +1,42 @@
 ﻿using DevApp.Business.Core.Models;
+using DevApp.Business.Core.Notificacoes;
 using FluentValidation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using FluentValidation.Results;
 
 namespace DevApp.Business.Core.Services
 {
-   public abstract class BaseService 
+    public abstract class BaseService
     {
         //Executa qualquer validação de qualquer entidade utilizando programação genérica
+        private readonly INotificador _notificador;
 
-                                                                        //TV tem que ser classe/herdar de AbstractValidator da entidade genérica que passei onde TE herda de Entity
-        protected bool ExecutarValidacao<TV, TE>(TV validacao, TE entidade) where TV: AbstractValidator<TE> where TE : Entity//Receber validação e a entidade e devolver o resultado.
+        protected BaseService(INotificador notificador)
         {
-            var validator = validacao.Validate(entidade);
+            _notificador = notificador;
+        }
 
-            return validator.IsValid;
+        protected void Notificar(ValidationResult validationResult) //Toda vez que executarmos uma validação, chamamos essa notificação
+        {
+            foreach (var error in validationResult.Errors) //Lista de erros
+            {
+                Notificar(error.ErrorMessage);
+            }
+        }
+        protected void Notificar(string mensagem) //Sobrecarga do método acima
+        {
+            _notificador.Handle(new Notificacao(mensagem)); //Criando e manipulando nova notificação
+        }
+
+        //TV tem que ser classe/herdar de AbstractValidator da entidade genérica que passei onde TE herda de Entity
+        protected bool ExecutarValidacao<TV, TE>(TV validacao, TE entidade) where TV : AbstractValidator<TE> where TE : Entity //Receber validação e a entidade e devolver o resultado.
+        {
+            var validator = validacao.Validate(entidade); //ValidationResult do prório asp.net(representação do erro em data annotation) e também do fluent validation
+
+            if (validator.IsValid) return true;
+
+            Notificar(validator);
+
+            return false;
         }
     }
 }
--- src/DevApp.Business/Models/Fornecedores/Services/FornecedorService.cs
+++ src/DevApp.Business/Models/Fornecedores/Services/FornecedorService.cs
@@ -1,9 +1,8 @@
-﻿using DevApp.Business.Core.Services;
+﻿using DevApp.Business.Core.Notificacoes;
+using DevApp.Business.Core.Services;
 using DevApp.Business.Models.Fornecedores.Validations;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace DevApp.Business.Models.Fornecedores.Services
@@ -13,7 +12,9 @@ namespace DevApp.Business.Models.Fornecedores.Services
         private readonly IFornecedorRepository _fornecedorRepository;
         private readonly IEnderecoRepository _enderecoRepository;
 
-        public FornecedorService(IFornecedorRepository fornecedorRepository, IEnderecoRepository enderecoRepository)
+        public FornecedorService(IFornecedorRepository fornecedorRepository,
+                                 IEnderecoRepository enderecoRepository,
+                                 INotificador notificador) : base(notificador)
         {
             _fornecedorRepository = fornecedorRepository;
             _enderecoRepository = enderecoRepository;
@@ -21,11 +22,12 @@ namespace DevApp.Business.Models.Fornecedores.Services
 
         public async Task Adicionar(Fornecedor fornecedor)
         {
-            if (!ExecutarValidacao(new FornecedorValidation(), fornecedor) || !ExecutarValidacao(new EnderecoValidation(), entidade: fornecedor.Endereco)) return;
+            if (!ExecutarValidacao(new FornecedorValidation(), fornecedor) //Quando executar a validação, internamente já notificamos o erro que ficara disponível na lista para pegarmos em outro momento.
+                || !ExecutarValidacao(new EnderecoValidation(), entidade: fornecedor.Endereco)) return;
 
             if (await FornecedorExistente(fornecedor)) return;
 
-            await _fornecedorRepository.Adicionar(fornecedor);       
+            await _fornecedorRepository.Adicionar(fornecedor);
         }
 
         public async Task Atualizar(Fornecedor fornecedor)
@@ -47,21 +49,28 @@ namespace DevApp.Business.Models.Fornecedores.Services
         public async Task<bool> FornecedorExistente(Fornecedor fornecedor)
         {
             var fornecedorAtual = await _fornecedorRepository.Buscar(f => f.Documento == fornecedor.Documento && f.Id != fornecedor.Id);
-                
-                return fornecedorAtual.Any();
+
+            if (!fornecedorAtual.Any()) return false;//Se não encontrar nenhum fornecedor, por que a validação é se o fornecedor existe.
+
+            Notificar("Já existe um fornecedor com este documento informadoi"); //Caso exista
+            return true;
         }
 
         public async Task Remover(Guid id)
         {
-
             var fornecedor = await _fornecedorRepository.ObterFornecedorProdutosEndereco(id);
 
-            if (fornecedor.Produtos.Any()) return; //Não deletar um fornecedor se tiver produtos.
-
-            if(fornecedor.Endereco != null)
+            if (fornecedor.Produtos.Any()) //Não deletar um fornecedor se tiver produtos.
             {
-                await _enderecoRepository.Remover(fornecedor.Endereco.Id); //Validar o endereço para remove-lo
+                Notificar("O fornecedor possui produtos cadastrados!");
+                return;
             }
+
+            if (fornecedor.Endereco != null)
+            {
+                await _enderecoRepository.Remover(fornecedor.Endereco.Id); //Validar o endereço para removê-lo
+            }
+
             await _enderecoRepository.Remover(id); //Após remover o endereço, remove o fornecedor
         }
 
